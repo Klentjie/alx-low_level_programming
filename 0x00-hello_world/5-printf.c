38ab554781d9ff1b4da544a11b000245008aaa44
Klentjie <anammagoxo@gmail.com>
2023-02-16 12:38:51 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int maini (void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
